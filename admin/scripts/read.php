55db261061e0c1e6b2be43c93b08033354c8e73b
Darren Le <darrenle98@gmail.com>
2020-03-31 22:01:13 -0400

    finished index and details pages

--- admin/scripts/read.php
+++ admin/scripts/read.php
@@ -1,45 +1,50 @@
-<?php 
-function getAll($tbl){
+<?php
+function getAll($tbl)
+{
     $pdo = Database::getInstance()->getConnection();
 
-    $queryAll = 'SELECT * FROM '.$tbl;
+    $queryAll = 'SELECT * FROM ' . $tbl;
     $results = $pdo->query($queryAll);
 
-    if($results){
+    if ($results) {
         return $results;
-    }else{
+    } else {
         return 'There was a problem accessing this info';
     }
 };
 
-function getSingleMovie($tbl,$col,$id){
+function getSingleMovie($tbl, $col, $id)
+{
     //TODO: refer the function above to finish this one
+
+    // 
     $pdo = Database::getInstance()->getConnection();
-    $query = "SELECT * FROM $tbl WHERE $col = $id";
-    
+    $query = "select p.*, GROUP_CONCAT(c.color_name) as color, g.gender_type as gender, r.review_scale as review, b.brand_name as brand from tbl_products p left join tbl_linking_color link ON link.product_id = p.product_id left join tbl_color c on c.color_id = link.color_id left join tbl_linking_gender link_g ON link_g.product_id = p.product_id left join tbl_gender g on g.gender_id = link_g.gender_id left join tbl_linking_review link_r on link_r.product_id = p.product_id left join tbl_review r on r.review_id = link_r.review_id left join tbl_linking_brand link_b on link_b.product_id = p.product_id left join tbl_brand b on b.brand_id = link_b.brand_id where p.product_id=$id;";
+
     $results = $pdo->query($query);
-    
-    if($results){
+
+    if ($results) {
         return $results;
-    }else{
+    } else {
         return 'There was a problem';
     }
 }
 
-function getMoviesByFilter($args){
+function getMoviesByFilter($args)
+{
     $pdo = Database::getInstance()->getConnection();
     $query = 'SELECT * FROM ' . $args['tbl'] . ' AS t,';
-    $query .= ' '. $args["tbl2"]. ' AS t2,';
-    $query .= ' '. $args['tbl3']. ' AS t3';
-    $query .=' WHERE t.'. $args['col']. ' = t3.' .$args['col'];
-    $query .= ' AND t2.' . $args['col2'].' = t3.'. $args['col2'];
-    $query .= ' AND t2.'.$args['col3']. ' ="'.$args['filter'].'"'; 
-    
-  
+    $query .= ' ' . $args["tbl2"] . ' AS t2,';
+    $query .= ' ' . $args['tbl3'] . ' AS t3';
+    $query .= ' WHERE t.' . $args['col'] . ' = t3.' . $args['col'];
+    $query .= ' AND t2.' . $args['col2'] . ' = t3.' . $args['col2'];
+    $query .= ' AND t2.' . $args['col3'] . ' ="' . $args['filter'] . '"';
+
+
     $results = $pdo->query($query);
-    if($results){
+    if ($results) {
         return $results;
-    }else{
+    } else {
         return 'There was a problem';
     }
 }
--- index.php
+++ index.php
@@ -1,43 +1,56 @@
 <?php
-    require_once 'load.php';
+require_once 'load.php';
 
-    if(isset($_GET['filter'])){
-        $args = array(
-            'tbl'=>'tbl_movies',
-            'tbl2'=>'tbl_genre',
-            'tbl3'=>'tbl_mov_genre',
-            'col'=>'movies_id',
-            'col2'=>'genre_id',
-            'col3'=>'genre_name',
-            'filter'=>$_GET['filter']
-        );
-        $getMovies = getMoviesByFilter($args);
-    }else{
-        $movie_table = 'tbl_movies';
-        $getMovies = getAll($movie_table);
-    }
-   
+if (isset($_GET['filter'])) {
+    $args = array(
+        'tbl' => 'tbl_movies',
+        'tbl2' => 'tbl_genre',
+        'tbl3' => 'tbl_mov_genre',
+        'col' => 'movies_id',
+        'col2' => 'genre_id',
+        'col3' => 'genre_name',
+        'filter' => $_GET['filter']
+    );
+    $getProducts = getMoviesByFilter($args);
+} else {
+    $products_table = 'tbl_products';
+    $getProducts = getAll($products_table);
+}
+
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
     <title>Welcome to the Movie CMS!</title>
 </head>
-<body>
+
+<style>
+    img {
+        object-fit: cover;
+        height: 300px !important;
+    }
+</style>
+
+<body class="container">
     <?php include 'templates/header.php'; ?>
- 
-    <?php while($row = $getMovies->fetch(PDO::FETCH_ASSOC)):?>
-        <div class="movie-item">
-            <img src="images/<?php echo $row['movies_cover'];?>" alt="<?php echo $row['movies_title'];?>"/>
-            <h2><?php echo $row['movies_title']; ?></h2>
-            <h4><?php echo $row['movies_year']; ?></h4>
-            <a href="details.php?id=<?php echo $row["movies_id"];?>">Read More</a>
-        </div>
-    <?php endwhile;?>
+    <div class="row">
+        <?php while ($row = $getProducts->fetch(PDO::FETCH_ASSOC)) : ?>
+            <div class="movie-item col-12 col-md-4 p-5">
+                <img class="img-fluid w-100" src="images/<?php echo $row['image']; ?>" alt="<?php echo $row['product_name']; ?>" />
+                <h2><?php echo $row['product_name']; ?></h2>
+                <h4><?php echo $row['price']; ?></h4>
+                <a href="details.php?id=<?php echo $row["product_id"]; ?>">Read More</a>
+            </div>
+        <?php endwhile; ?>
+    </div>
     <?php include 'templates/footer.php'; ?>
 </body>
+
 </html>
